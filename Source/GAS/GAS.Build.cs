7a9158b5510f430b46b653fcd3934c38bc26dfb3
jasonwwu <jasonwwu@tencent.com>
2025-06-21 18:19:34 +0800

    first commit

---
+++ Source/GAS/GAS.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GAS : ModuleRules
+{
+	public GAS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"GAS",
+			"GAS/Variant_Platforming",
+			"GAS/Variant_Combat",
+			"GAS/Variant_Combat/AI",
+			"GAS/Variant_SideScrolling",
+			"GAS/Variant_SideScrolling/Gameplay",
+			"GAS/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
